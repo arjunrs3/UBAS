f5a6aff312f69fa1fc2c23d56d1dd92d6f99047c
arshah2 <119633165+arjunrs3@users.noreply.github.com>
2025-02-15 16:56:44 -0800

    added documentation config file

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'UBAS'
+author = 'Arjun Shah'
+
+version = '1.0'
+release = '1.0'
+
+extensions = ['numpydoc',
+              'intersphinx',
+              'autodoc',
+              'extlinks',
+              'autosectionlabel',
+              'napoleon']
+
+numfig = True
